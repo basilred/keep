e1642bbf808c1e0ea474e6478569634f7fe9c417
Vassily Krasnov <mykrasnov@gmail.com>
2019-10-08 23:28:43 +0300

    Add update route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -35,4 +35,17 @@ module.exports = function(app, db) {
       }
     });
   });
+
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').updateOne(details, note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occured' });
+      } else {
+        res.send(note);
+      }
+    });
+  });
 };
